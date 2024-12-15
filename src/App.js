e989dddc9b4caba386bc3b5eb935815cd4578487
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-12-15 22:01:42 +0000

    update Home, App

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
 import Header from './components/Header';
 import './scss/app.scss';
-import { db } from "./firebaseConfig";
-import { getDocs, collection } from "firebase/firestore";
-import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Auth from './components/pages/Auth';
 import Home from './components/pages/Home';
-import PizzaBlock from './components/PizzaBlock';
-import { Skeleton } from './components/Skeleton';
 
 
 function App() {
-
-  const [pizzaList, setpizzaList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const pizzaCollectionRef = collection(db, "pizza");
-
-  const getPizzaList = async () => {
-    try {
-      const data = await getDocs(pizzaCollectionRef);
-
-      const filteredData = data.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
-      setIsLoading(false);
-      setpizzaList(filteredData);
-
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  useEffect(() => {
-    getPizzaList();
-  }, []);
 
 
   return (
@@ -50,18 +20,7 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path="/login" element={<Auth />} />
           </Routes>
-          <div class="content__items">
-            {isLoading ? [...Array(3)].map(() => <Skeleton />) : pizzaList.map((obj) => (
-              <PizzaBlock
-                title={obj.name}
-                image={obj.image}
-                price={obj.price}
-                sizes={obj.sizes}
-                types={obj.types}
-                loading={isLoading} />
-            ))
-            }
-          </div>
+
         </Router>
 
       </div>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,40 @@
 import Categories from '../Categories';
 import Sort from '../Sort';
+import { Skeleton } from '../Skeleton';
+import PizzaBlock from '../PizzaBlock';
+
+import { db } from "../../firebaseConfig";
+import { getDocs, collection } from "firebase/firestore";
+import { useEffect, useState } from 'react';
+
 
 const Home = () => {
+
+    const [pizzaList, setpizzaList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const pizzaCollectionRef = collection(db, "pizza");
+
+    const getPizzaList = async () => {
+        try {
+            const data = await getDocs(pizzaCollectionRef);
+
+            const filteredData = data.docs.map((doc) => ({
+                ...doc.data(),
+                id: doc.id,
+            }));
+            setIsLoading(false);
+            setpizzaList(filteredData);
+
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    useEffect(() => {
+        getPizzaList();
+    }, []);
+
 
     return (
         <div class="content">
@@ -9,10 +42,20 @@ const Home = () => {
                 <div class="content__top">
                     <Categories />
                     <Sort />
-
                 </div>
                 <h2 class="content__title">All Pizzas</h2>
-
+                <div class="content__items">
+                    {isLoading ? [...Array(3)].map(() => <Skeleton />) : pizzaList.map((obj) => (
+                        <PizzaBlock
+                            title={obj.name}
+                            image={obj.image}
+                            price={obj.price}
+                            sizes={obj.sizes}
+                            types={obj.types}
+                            loading={isLoading} />
+                    ))
+                    }
+                </div>
             </div>
         </div>
 
