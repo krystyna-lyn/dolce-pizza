0ed7e83969f59e53e76444efb98ae63f439cea99
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2025-01-09 19:00:21 +0000

    update Auth

--- src/components/pages/Auth.js
+++ src/components/pages/Auth.js
@@ -1,35 +1,81 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react";
-import { auth } from "../../firebaseConfig";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { auth, googleProvider } from "../../firebaseConfig";
+import { useNavigate } from "react-router-dom";
 
 
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [user, setUser] = useState(null);
+
+    //console.log(auth?.currentUser?.email)
+
+    const navigate = useNavigate();
 
     const singnIn = async () => {
-        await createUserWithEmailAndPassword(auth, email, password)
+        try {
+            const createdUser = await createUserWithEmailAndPassword(auth, email, password);
+            console.log(createdUser);
+            navigate('/home')
+        } catch (err) {
+            console.error(err);
+            navigate('/notfound')
+        }
     }
 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser); // Update the user state when authentication state changes
+        });
+        return () => unsubscribe(); // Clean up the listener on component unmount
+    }, []);
+
+
+    const googleSignIn = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    const logout = async () => {
+        try {
+            await signOut(auth);
+        } catch (err) {
+            console.error(err);
+        }
+    }
 
     return (
+
         <div class="login-content">
-            <h1>Login</h1>
-            <form class='form'>
-                <input type="email"
-                    placeholder="Email"
-                    required
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <input type="password"
-                    placeholder="Password"
-                    required
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button class="button" onClick={singnIn}>Go</button>
-            </form>
+            {user ? (
+                <div>
+                    <h1>Welcome, {user.email || "User"}!</h1>
+                    <button className="button" onClick={logout}>Logout</button>
+                </div>
+            ) : (
+                <>
+                    <h1>Login</h1><form class='form'>
+                        <input type="email"
+                            placeholder="Email"
+                            required
+                            onChange={(e) => setEmail(e.target.value)} />
+                        <input type="password"
+                            placeholder="Password"
+                            required
+                            onChange={(e) => setPassword(e.target.value)} />
+                        <button class="button" onClick={singnIn}>Sign in</button>
+                        <button class="button" onClick={googleSignIn}>with Google</button>
+                    </form>
+                </>
+            )}
+
         </div>
+
     )
 }
 
