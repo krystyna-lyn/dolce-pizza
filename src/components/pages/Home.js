0b48cfaf0cc77566c492baf329c8b6c25d786a87
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2025-02-23 22:07:01 +0000

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,7 +7,6 @@ import { db } from "../../firebaseConfig";
 import { getDocs, collection, query, orderBy, where, limit, startAfter } from "firebase/firestore";
 import { useEffect, useRef, useState } from 'react';
 import Pagination from '../Pagination';
-import qs from 'qs';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { setCategoryId } from '../../redux/slices/categorySlice';
@@ -111,15 +110,6 @@ const Home = () => {
         window.scrollTo(0, 0);
     }, [categoryId, sortField, sortOrder, currentPage, searchValue]);
 
-    useEffect(() => {
-        const queryString = qs.stringify({
-            sortField,
-            categoryId,
-            currentPage
-        })
-        //console.log(queryString)
-        navigate(`?${queryString}`);
-    }, [sortField, categoryId, currentPage])
 
     const skeleton = [...Array(8)].map((_, index) => <Skeleton key={index} />);
 
